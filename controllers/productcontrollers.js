f18ed4e4a4301dfbb67007ba7d3e0daf9a193162
Harshana Bhandari <np03cs4a230186@heraldcollege.edu.np>
2025-06-26 13:32:14 +0545

    fix: fix error in filter product by name feature

--- controllers/productcontrollers.js
+++ controllers/productcontrollers.js
@@ -94,17 +94,22 @@ async function filterproductbycategoriesanstock(req,reply){
     reply.code(500).send({ error: 'Failed to filter products' });
   }
 }
-async function filterproductbyname(req,reply){
-  try{
-    const products=Product.find({ name: { $regex: req.params.name, $options: 'i' } });
-    if(products.length==0){ 
+async function filterproductbyname(req, reply) {
+  try {
+    const products = await Product.find({
+      name: { $regex: req.params.name, $options: 'i' }
+    });
+
+    if (products.length === 0) {
       return reply.code(404).send({ error: 'No products found with this name' });
-    } 
+    }
+
     reply.send(products);
 
-  }catch (error) {
+  } catch (error) {
     req.log.error(error);
     reply.code(500).send({ error: 'Failed to filter products by name' });
   }
 }
+
 module.exports={createProduct,getallProducts,getProductbyId,updateProduct,deleteProduct,getProductbyCategory,filterproductsbycategoriesandprice,filterproductbycategoriesanstock,filterproductbyname};
