176685cbdace05ec8188a9cd206ef2c5c25a64b7
Harshana Bhandari <np03cs4a230186@heraldcollege.edu.np>
2025-06-29 12:46:36 +0545

    feat:Heading is added in page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Heading from './Heading.jsx'
+import Field from './field.jsx'
 
 function App() {
-  
+  return (
+    <div>
+      <Heading></Heading>
+      <Field></Field>
+    </div>
+  )
 }
 
 export default App
